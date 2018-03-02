ad4e04095db129f227d222d90bc6eae50425823f
Davrod29 <davidrod805@gmail.com>
2018-03-01 21:37:23 -0700

    added a local host route

--- models/project.js
+++ models/project.js
@@ -1,28 +1,8 @@
-// Dependencies
-// =============================================================
-
-// This may be confusing but here Sequelize (capital) references the standard library
-var Sequelize = require("sequelize");
-// sequelize (lowercase) references our connection to the DB.
-var sequelize = require("../config/connection.js");
-
-// Creates a "Chirp" model that matches up with DB
-var Project = sequelize.define("project", {
-  author: {
-    type: Sequelize.STRING
-  },
-  body: {
-    type: Sequelize.STRING
-  },
-  created_at: {
-    type: Sequelize.DATE
-  }
-}, {
-  timestamps: false
-});
-
-// Syncs with DB
-Project.sync();
-
-// Makes the Project Model available for other files (will also create a table)
-module.exports = Project;
+module.exports = function (sequelize, DataTypes) {
+  var Projects = sequelize.define("projects", {
+    author: DataTypes.STRING,
+    task: DataTypes.STRING,
+    dueDate: DataTypes.DATE
+  });
+  return Projects;
+};
